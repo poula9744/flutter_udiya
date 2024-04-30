b6607968f7cd31ea03dc0eae5a75e882e39b6130
Ksusie0214 <tdlrtmvldj@gmail.com>
2024-04-30 11:23:50 +0900

    김수빈

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -6,8 +7,50 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("UDIYA"),),
-      body: Container(),
+      appBar: AppBar(title: Text("UDIYA",style: TextStyle(color: Color(0xff243c84), fontWeight: FontWeight.bold),),
+          iconTheme: IconThemeData(color: Color(0xff243c84)),
+          actions: [
+            IconButton(onPressed: (){
+              print("QR코드 클릭");
+            }, icon: Icon(Icons.qr_code)),
+          ]),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(25, 5, 0, 0),
+                  child: Text("추억의 달달커피로", style: TextStyle(fontSize: 20),),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                    margin: EdgeInsets.fromLTRB(25, 0, 0, 0),
+                    child: Text("달달-한 당 충전 시간", style: TextStyle(fontSize: 20),)
+                ),
+                Container(
+                    child: Icon(Icons.favorite,color: Color(0xffb40000))
+                ),
+              ],
+            ),
+            Container(
+              width: 340,
+              height: 160,
+              child: Image.asset(
+                'assets/images/main_banner.jpg',
+                width: 340,
+                height: 160,
+                fit: BoxFit.cover,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
