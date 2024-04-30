23ce88ec9a76d6e9ab8495eb8370b1f6bc53f4d4
hyeon0714 <xodid6960@naver.com>
2024-04-30 16:53:44 +0900

    재현연결

--- lib/JaeHyeon/jaehyeon3.dart
+++ lib/JaeHyeon/jaehyeon3.dart
@@ -155,7 +155,9 @@ class JeaHyeon3 extends StatelessWidget {
             width: 240,
             decoration:BoxDecoration(borderRadius: BorderRadius.circular(30),color: Color(0xff243C84),),
             child: TextButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.pushNamed(context, "/youngsoo");
+              },
               child: Text(
                 "주문하러 가기",
                 style: TextStyle(color: Color(0xffffffff), fontWeight: FontWeight.w600),
